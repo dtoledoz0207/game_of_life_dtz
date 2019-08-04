689f22647bcbcc3dbb821bc2391b1ad4fb1ef322
David Toledo <dtoledoz0207@gmail.com>
2019-08-04 13:22:20 -0500

    initial-commit

---
+++ lib/game_of_life_dtz.rb
@@ -0,0 +1,6 @@
+require "game_of_life_dtz/version"
+
+module GameOfLifeDtz
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/game_of_life_dtz/version.rb
@@ -0,0 +1,3 @@
+module GameOfLifeDtz
+  VERSION = "0.1.0"
+end
