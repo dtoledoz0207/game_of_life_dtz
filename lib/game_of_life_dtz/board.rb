ba72361034975182aa9722cf1e2b928d67a4582b
David Toledo <dtoledoz0207@gmail.com>
2019-08-04 13:48:50 -0500

    class-board-Added

---
+++ lib/game_of_life_dtz/board.rb
@@ -0,0 +1,54 @@
+class Board
+  attr_reader :dimension, :board, :board_copy
+  def initialize(dimension)
+    @dimension = dimension
+    create_board(@dimension)
+  end
+
+  def create_board(dimension)
+    @board = []
+    dimension.times do
+      @board << Array.new(dimension, 0)
+    end
+    @board.each do |row|
+      row.map! do |column|
+        column = rand(2)
+      end
+    end
+  end
+
+  def show
+    @board.each do |row|
+      row.each do |column|
+        print column == 1 ? '@ ' : '. '
+      end
+      puts ' '
+    end
+    puts ' '
+  end
+
+  def new_generation(number_of_neighbors, index_row, index_column)
+    if number_of_neighbors < 2 || number_of_neighbors > 3
+      @board_copy[index_row][index_column] = 0
+    elsif number_of_neighbors == 3
+      @board_copy[index_row][index_column] = 1
+    end
+  end
+
+  def search_neighbors
+    @board_copy = []
+    @board_copy = Marshal.load(Marshal.dump(@board))
+
+    @board.each_with_index do |row, index_row|
+      row.each_index do |index_column|
+        number_of_neighbors = 0
+        cell = Cell.new
+        number_of_neighbors += cell.count_neighbors_above_below(@board, index_row, index_column)
+        number_of_neighbors += cell.count_neighbors_left_right(@board, index_row, index_column)
+        new_generation(number_of_neighbors, index_row, index_column)
+      end
+    end
+
+    @board = Marshal.load(Marshal.dump(@board_copy))
+  end
+end
