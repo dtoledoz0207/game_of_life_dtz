c546eedb960b849b60fc56f3adafbaf20664cb4f
David Toledo <dtoledoz0207@gmail.com>
2019-08-05 11:44:00 -0500

    count-neighbors-left-right-test-Added

--- spec/game_of_life_dtz_spec.rb
+++ spec/game_of_life_dtz_spec.rb
@@ -35,4 +35,41 @@ RSpec.describe Cell do
     end
   end
 
+  describe '#count_neighbors_left_right' do
+    context 'When the cell has one neighbor on the left and right' do
+      it 'respond with the number 1' do
+        cel = Cell.new
+        board = [[0, 0, 0, 1, 1], [0, 0, 1, 1, 1], [0, 0, 1, 0, 1], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+        number_of_neighbors = cel.count_neighbors_left_right(board, 0, 3)
+        expect(number_of_neighbors).to be(1)
+      end
+    end
+    context 'When the cell has two neighbors on the left and right' do
+      it 'respond with the number 2' do
+        cel = Cell.new
+        board = [[0, 0, 1, 1, 1], [0, 0, 1, 1, 1], [0, 0, 1, 0, 1], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+        number_of_neighbors = cel.count_neighbors_left_right(board, 0, 3)
+        expect(number_of_neighbors).to be(2)
+      end
+    end
+    context 'When the cell has zero neighbors on the left and right' do
+      it 'respond with the number 0' do
+        cel = Cell.new
+        board = [[0, 0, 0, 1, 0], [0, 0, 1, 1, 1], [0, 0, 1, 0, 1], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+        number_of_neighbors = cel.count_neighbors_left_right(board, 0, 3)
+        expect(number_of_neighbors).to be(0)
+      end
+    end
+  end
+
+  context 'When the cell has three neighbors around it' do
+    it 'respond with the number 3' do
+      cel = Cell.new
+      number_of_neighbors = 0
+      board = [[0, 0, 0, 1, 1], [0, 0, 1, 0, 1], [0, 0, 1, 0, 1], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+      number_of_neighbors += cel.count_neighbors_above_below(board, 0, 3)
+      number_of_neighbors += cel.count_neighbors_left_right(board, 0, 3)
+      expect(number_of_neighbors).to be(3)
+    end
+  end
 end
